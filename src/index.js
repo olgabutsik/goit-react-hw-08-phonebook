8b1f694e04a809210b434ebb738c298658ec1e1d
Olha <cukbu30@gmai.com>
2023-02-28 09:47:25 +0200

    updated store.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { App } from 'components/App';
+import { store, persistor } from './redux/store';
 import { Global } from '@emotion/react';
 import { GlobalStyles } from './components/Styles/GlobalStyles';
-import { App } from 'components/App';
+
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Global styles={GlobalStyles} />
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Global styles={GlobalStyles} />
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
